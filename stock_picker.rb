4e5a05bd1fe34e66cf6b56bd2be4652e9efb2efa
HSaad <hagrsaad16@gmail.com>
2018-02-21 14:26:37 -0500

    Added for loop

--- stock_picker.rb
+++ stock_picker.rb
@@ -4,15 +4,26 @@ def stock_picker(array)
 	sell_date = 0;
 	
 	array.each_index do |index|
-		if(array[index + 1])
+		if (index != 0)
+			puts array[index] - array[index - 1]
+		end
 	end
-	puts array.max
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
 
-#first find max numb in array 
-#find difference between that num and all numbers before it
-#if difference > than max, change buy and sell
-#then find next max numb in array and repeat
-#continue until next max num in array is not greater than
+
+=begin
+start from beginning of array
+calculate the difference between 
+that num and every number before it
+if diff > max diff, set new max, buy,sell	
+	
+
+first find max numb in array 
+find difference between that num and all numbers before it
+if difference > than max, change buy and sell
+then find next max numb in array and repeat
+continue until next max num in array is not greater than
+
+=end
